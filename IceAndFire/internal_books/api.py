138e80fa52292c2cd35e26418dea80fbfd6c47d3
ahmadx16 <ahmadx16@hotmail.com>
2020-08-25 00:53:44 +0500

    CRD operation with ViewSet

--- IceAndFire/internal_books/api.py
+++ IceAndFire/internal_books/api.py
@@ -1,8 +1,8 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets
 from rest_framework.response import Response
-from rest_framework.parsers import JSONParser
+from django.shortcuts import get_object_or_404
 
-from internal_books.models import Book
+from internal_books.models import Book, Author
 from .serializers import BookSerializer
 
 
@@ -18,62 +18,60 @@ class ResponseInfo(object):
             self.response.pop("message")
 
 
-class BookViewSet(viewsets.ModelViewSet):
+class BookViewSet(viewsets.ViewSet):
     """Book ViewSet for handeling internal books"""
 
-    def __init__(self, **kwargs):
-        self.response_format = ResponseInfo().response
-        super(BookViewSet, self).__init__(**kwargs)
-
     queryset = Book.objects.all()
-    permissions_classes = [
-        permissions.AllowAny
-    ]
-    serializer_class = BookSerializer
+    # permissions_classes = [
+    #     permissions.AllowAny
+    # ]
 
-    def create(self, request, *args, **kwargs):
-        # authors = request.data.pop('authors')
-        # print(authors)
-        create_response = super(BookViewSet, self).create(request, *args, **kwargs)
+    def create(self, request):
+        authors = request.data.pop('authors')
+        book = Book.objects.create(**request.data)
+        book_serialized = BookSerializer(book)
+        for author in authors:
+            Author.objects.create(book=book, name=author)
         response_format = ResponseInfo(status_code=201,
                                        status="success",
-                                       data=create_response.data).response
+                                       data=book_serialized.data).response
         return Response(response_format)
 
     def list(self, request, *args, **kwargs):
-        list_response = super(BookViewSet, self).list(request, *args, **kwargs)
+        serializer = BookSerializer(self.queryset, many=True)
         response_format = ResponseInfo(status_code=200,
                                        status="success",
-                                       data=list_response.data).response
+                                       data=serializer.data).response
         return Response(response_format)
 
-    def update(self, request, *args, **kwargs):
-        update_response = super(BookViewSet, self).update(request, *args, **kwargs)
-        book_name = update_response.data["name"]
-        message = f"The book {book_name} was updated successfully"
-        response_format = ResponseInfo(status_code=200,
-                                       status="success",
-                                       message=message,
-                                       data=update_response.data).response
+    # def update(self, request, *args, **kwargs):
+    #     update_response = super(BookViewSet, self).update(request, *args, **kwargs)
+    #     book_name = update_response.data["name"]
+    #     message = f"The book {book_name} was updated successfully"
+    #     response_format = ResponseInfo(status_code=200,
+    #                                    status="success",
+    #                                    message=message,
+    #                                    data=update_response.data).response
 
-        return Response(response_format)
+    #     return Response(response_format)
 
-    def destroy(self, request, *args, **kwargs):
-
-        book_instance = self.get_object()
-        book_name = self.get_serializer(book_instance).data["name"]
-        message = f"The book {book_name} was deleted successfully"
-        destroy_response = super(BookViewSet, self).destroy(request, *args, **kwargs)
+    def destroy(self, request, pk=None):
+        book = get_object_or_404(self.queryset, pk=pk)
+        message = f"The book {str(book.name)} was deleted successfully"
+        authors = book.authors.all()
+        for author in authors:
+            author.delete()
+        book.delete()
         response_format = ResponseInfo(status_code=200,
                                        status="success",
                                        message=message).response
-
         return Response(response_format)
 
-    def retrieve(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance)
-        self.response_format["status_code"] = 200
-        self.response_format["status"] = "success"
-        self.response_format["data"].append(serializer.data)
-        return Response(self.response_format)
+    def retrieve(self, request, pk=None):
+        book = get_object_or_404(self.queryset, pk=pk)
+        book_serialized = BookSerializer(book)
+
+        response_format = ResponseInfo(status_code=200,
+                                       status="success",
+                                       data=book_serialized.data).response
+        return Response(response_format)
