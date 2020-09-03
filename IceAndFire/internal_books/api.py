9cb74a1d3b298710ec8016449ee3938a97d4e284
ahmadx16 <ahmadx16@hotmail.com>
2020-09-03 21:32:31 +0500

    initial Generic views

--- IceAndFire/internal_books/api.py
+++ IceAndFire/internal_books/api.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, generics
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from django.db.models import Q
@@ -21,68 +21,29 @@ class ResponseInfo(object):
             self.response.pop("message")
 
 
-class BookViewSet(viewsets.ViewSet):
-    """Book ViewSet for handeling internal books"""
+class BookListCreateView(generics.ListCreateAPIView):
+    """For handeling internal books List, Create functionality"""
 
-    def create(self, request):
-        book_serializer = BookSerializer(data=request.data)
-        if book_serializer.is_valid():
-            book_serializer.save()
-            formatted_response = ResponseInfo(status_code=201,
-                                              status="success",
-                                              data=book_serializer.data).response
-            return Response(formatted_response, status=status.HTTP_201_CREATED)
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
-        return Response(book_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def list(self, request):
-        search_str = request.query_params.get('search_str', "")
-        books_query = Book.objects.filter(Q(name__icontains=search_str)
-                                          | Q(country__icontains=search_str)
-                                          | Q(publisher__icontains=search_str)
-                                          | Q(release_date__startswith=search_str)
-                                          )
-        serialized_books = BookSerializer(books_query, many=True)
+    def list(self, request, *args, **kwargs):
+        books_response = super().list(request, *args, **kwargs)
         formatted_response = ResponseInfo(status_code=200,
                                           status="success",
-                                          data=serialized_books.data).response
+                                          data=books_response.data).response
         return Response(formatted_response)
 
-    def partial_update(self, request, pk=None):
-        book = get_object_or_404(Book, pk=pk)
-        book_name = str(book.name)
-
-        book_serializer = BookSerializer(book, data=request.data, partial=True)
-        if book_serializer.is_valid():
-            book_serializer.save()
-            message = f"The book {str(book_name)} was updated successfully"
-
-            formatted_response = ResponseInfo(status_code=200,
-                                              status="success",
-                                              message=message,
-                                              data=book_serializer.data).response
-            return Response(formatted_response)
-
-        return Response(book_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def destroy(self, request, pk=None):
-        book = get_object_or_404(Book, pk=pk)
-        book_name = book.name
-        authors = list(book.authors.all())
-        book.delete()
-        for author in authors:
-            author.remove_extra_author()
-        message = f"The book {str(book_name)} was deleted successfully"
-        formatted_response = ResponseInfo(status_code=200,
+    def create(self, request, *args, **kwargs):
+        books_response = super().create(request, *args, **kwargs)
+        formatted_response = ResponseInfo(status_code=201,
                                           status="success",
-                                          message=message).response
+                                          data=books_response.data).response
         return Response(formatted_response)
 
-    def retrieve(self, request, pk=None):
-        book = get_object_or_404(Book, pk=pk)
-        book_serializer = BookSerializer(book)
 
-        formatted_response = ResponseInfo(status_code=200,
-                                          status="success",
-                                          data=book_serializer.data).response
-        return Response(formatted_response)
+class BookRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    """For handeling internal books Retrieve, Update, Destroy functionality"""
+
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
