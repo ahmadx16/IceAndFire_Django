7582301d30dc853cd6f9ca0ba94d02c2cf8fc0a9
ahmadx16 <ahmadx16@hotmail.com>
2020-09-04 08:04:07 +0500

    correct response format

--- IceAndFire/internal_books/api.py
+++ IceAndFire/internal_books/api.py
@@ -21,11 +21,15 @@ class ResponseInfo(object):
             self.response.pop("message")
 
 
-class BookListCreateView(generics.ListCreateAPIView):
-    """For handeling internal books List, Create functionality"""
+class BookQuerySet:
+    """Defines queryset and serializer class for the book"""
 
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+
+class BookListCreateView(BookQuerySet, generics.ListCreateAPIView):
+    """For handeling internal books List, Create functionality"""
 
     def list(self, request, *args, **kwargs):
         books_response = super().list(request, *args, **kwargs)
@@ -39,11 +43,34 @@ class BookListCreateView(generics.ListCreateAPIView):
         formatted_response = ResponseInfo(status_code=201,
                                           status="success",
                                           data=books_response.data).response
-        return Response(formatted_response)
+        return Response(formatted_response, status=status.HTTP_201_CREATED)
 
 
-class BookRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+class BookRetrieveUpdateDestroyView(BookQuerySet, generics.RetrieveUpdateDestroyAPIView):
     """For handeling internal books Retrieve, Update, Destroy functionality"""
 
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+    def retrieve(self, request, *args, **kwargs):
+        books_response = super().retrieve(request, *args, **kwargs)
+        formatted_response = ResponseInfo(status_code=200,
+                                          status="success",
+                                          data=books_response.data).response
+        return Response(formatted_response)
+
+    def update(self, request, *args, **kwargs):
+        books_response = super().update(request, *args, **kwargs)
+        message = "The book {} was updated successfully".format(str(books_response.data['name']))
+        formatted_response = ResponseInfo(status_code=200,
+                                          status="success",
+                                          message=message,
+                                          data=books_response.data).response
+        return Response(formatted_response)
+
+    def destroy(self, request, *args, **kwargs):
+        book_instance = self.get_object()
+        book_name = str(book_instance.name)
+        super().destroy(request, *args, **kwargs)
+        message = f"The book {book_name} was deleted successfully"
+        formatted_response = ResponseInfo(status_code=200,
+                                          status="success",
+                                          message=message).response
+        return Response(formatted_response)
